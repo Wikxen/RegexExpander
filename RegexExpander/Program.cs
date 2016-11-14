c772dac214a4a2db0e1a669aeb7db6b207dea39a
Erik Wikström <wikxen@gmail.com>
2016-11-14 14:21:53 +0100

    Added input handling

--- RegexExpander/Program.cs
+++ RegexExpander/Program.cs
@@ -10,6 +10,19 @@ namespace RegexExpander
     {
         static void Main(string[] args)
         {
+            if (args.Count() < 1)
+            {
+                Console.WriteLine("No input");
+                return;
+            }
+
+            string input = args[0];
+
+            if (input.IndexOf('[') < 0)
+            {
+                Console.WriteLine("No regex");
+                return;
+            }
         }
     }
 }
