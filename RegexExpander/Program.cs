8e6b737a2941f92569ce235db68e9a5ed86d8d38
Erik Wikström <wikxen@gmail.com>
2016-11-14 14:23:01 +0100

    Added regex extraction

--- RegexExpander/Program.cs
+++ RegexExpander/Program.cs
@@ -23,6 +23,36 @@ namespace RegexExpander
                 Console.WriteLine("No regex");
                 return;
             }
+
+            string stub = input.Substring(0, input.IndexOf('['));
+            string regex = input.Substring(input.IndexOf('[') + 1);
+            regex = regex.Substring(0, regex.Length - 1);
+            string[] regexes = regex.Split(new string[] { "][" }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<List<char>> parts = new List<List<char>>();
+            foreach (string rx in regexes)
+            {
+                if (rx.Contains("-"))
+                {
+                    List<char> bits = new List<char>();
+
+                    char start = rx.ElementAt(rx.IndexOf('-') - 1);
+                    char end = rx.ElementAt(rx.IndexOf('-') + 1);
+
+                    while (start != end)
+                    {
+                        bits.Add(start);
+                        start++;
+                    }
+                    bits.Add(end);
+
+                    parts.Add(bits);
+                }
+                else
+                {
+                    parts.Add(new List<char>(rx.ToCharArray()));
+                }
+            }
         }
     }
 }
